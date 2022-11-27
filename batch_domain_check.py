ccf11a74e859f6039c52b76ff62bedc66e326ddf
Yuan Tian <tianyuan@seaeverit.com>
2022-11-26 21:33:39 -0500

    update chinese college auth data

--- batch_domain_check.py
+++ batch_domain_check.py
@@ -24,12 +24,23 @@ def batch_processing(input_file, output_file):
     df = pd.DataFrame(d)
     df.to_csv(output_file)
     print(pd.read_csv(output_file))
-    
+
+def optimize_csv(univ_csv, auth_csv, output):
+    """
+    add features: univeristy name, sorting by authentication levels
+    """
+    univ_df = pd.read_csv(univ_csv)
+    auth_df = pd.read_csv(auth_csv)
+    result_df = univ_df.join(auth_df, lsuffix="_other")[["name", "domain", "SPF", "DMARC", "DKIM"]]
+    result_df.to_csv(output)
+
 if __name__ == "__main__":
-    # input = "test.txt"
-    # output = "test.csv"
-    input = "Chinese_college_domains.txt"
-    output = "Chinese_college_authentication.csv"
-    batch_processing(input, output)
-            
+    # input = "test.csv"
+    # output = "test_out.csv"
+    univ_csv = "domainList.csv"
+    auth_csv = "Chinese_college_authentication.csv"
+    output = "new_college_auth.csv"
+    optimize_csv(univ_csv, auth_csv, output)
+    
+    
 
